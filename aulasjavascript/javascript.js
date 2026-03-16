af1da8b4e0d102efe5c081750c8f65c41b417677
emerson <emers733@gmail.com>
2026-03-15 23:31:10 -0400

    objetos

--- aulasjavascript/javascript.js
+++ aulasjavascript/javascript.js
@@ -8,3 +8,22 @@ const mystring = `meu texto junto com variavel ${idade}`;
 console.log(mystring);
 
 // exemplo de OBJETOS
+const meuObjeto={
+    nome:"emerson",
+    idade:10,
+    // posso colocar outro objeto dentro
+    endereco:{
+        rua:"manaus das flores",
+        cidade:"manaus"
+    }
+}
+// Acessando a propriedade
+    console.log(meuObjeto); //aqui traz todas as propriedades do objeto
+    console.log(meuObjeto.nome); // aqui traz somente uma propriedade
+    console.log(meuObjeto.endereco.cidade); // acessa o objeto dentro do outro objeto
+
+    //alterar valor
+    meuObjeto.endereco.cidade= "itacoatiara";
+    console.log(meuObjeto);
+
+    
