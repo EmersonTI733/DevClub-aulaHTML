fa18c6c7afd29e60ff2b5f1cfda691c83ac67753
emerson <emers733@gmail.com>
2026-03-15 23:09:42 -0400

    inicio das aulas java script

---
+++ aulasjavascript/javascript.js
@@ -0,0 +1,10 @@
+// exemplo para uso do console
+console.log("oi, eu estou no console");
+
+// exemplo de tamplate string `` crase ivertida é possivel inserir variaveis ${variavel}
+const idade= 20;
+const mystring = `meu texto junto com variavel ${idade}`;
+// teste
+console.log(mystring);
+
+// exemplo de OBJETOS
