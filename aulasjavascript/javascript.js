ba13d8b66ad0f6c6f9c1e7e3b1babee4f6ccdfb8
emerson <emers733@gmail.com>
2026-03-18 19:28:27 -0400

    manipulando dom

--- aulasjavascript/javascript.js
+++ aulasjavascript/javascript.js
@@ -1,43 +1,74 @@
+// execute o codigo aqui com ctrl+alt+N, mas somente vai funcionar para os codigos java script
 // exemplo para uso do console
 console.log("oi, eu estou no console");
 
 // exemplo de tamplate string `` crase ivertida é possivel inserir variaveis ${variavel}
-const idade= 20;
+const idade = 20;
 const mystring = `meu texto junto com variavel ${idade}`;
 // teste
 console.log(mystring);
 
 // exemplo de OBJETOS
-const meuObjeto={
-    nome:"emerson",
-    idade:10,
+const meuObjeto = {
+    nome: "emerson",
+    idade: 10,
     // posso colocar outro objeto dentro
-    endereco:{
-        rua:"manaus das flores",
-        cidade:"manaus"
+    endereco: {
+        rua: "manaus das flores",
+        cidade: "manaus"
     }
 }
 // Acessando a propriedade
-    console.log(meuObjeto); //aqui traz todas as propriedades do objeto
-    console.log(meuObjeto.nome); // aqui traz somente uma propriedade
-    console.log(meuObjeto.endereco.cidade); // acessa o objeto dentro do outro objeto
+console.log(meuObjeto); //aqui traz todas as propriedades do objeto
+console.log(meuObjeto.nome); // aqui traz somente uma propriedade
+console.log(meuObjeto.endereco.cidade); // acessa o objeto dentro do outro objeto
 
-    //alterar valor
-    meuObjeto.endereco.cidade= "itacoatiara";
-    console.log(meuObjeto);
+//alterar valor
+meuObjeto.endereco.cidade = "itacoatiara";
+console.log(meuObjeto);
 
 
 // vamos trabalha aqui o DOM com javasript
 // Document Object Model
 // exemplo getElementById
-    const input= document.getElementById("input-nome");
-    console.log(input);
+// OBS: a partir dequi o codigo para manipular a dom so funciona no navegador
+const input = document.getElementById("input-nome");
+console.log(input);
 
-    const inputclasse= document.getElementsByClassName("inputclass");
-    console.log(inputclasse);
+const inputclasse = document.getElementsByClassName("inputclass");
+console.log(inputclasse);
 
-    const paragrafos= document.getElementsByTagName("p");
-    console.log(paragrafos);
+const paragrafos = document.getElementsByTagName("p");
+console.log(paragrafos);
 
-    const pegueName= document.getElementsByName("endereco");
-    console.log(pegueName);
+const pegueName = document.getElementsByName("endereco");
+console.log(pegueName);
+
+// o queryselector pega o primero elemento somente.
+const elementos = document.querySelector("#input-nome");
+console.log(elementos);
+
+//aqui vai trazer todos os elementos
+const elements = document.querySelectorAll("form");
+console.log(elements);
+
+// AGORA VEJA O SE PODDE FAZER COM ISSO
+
+// ja consigo manipular a DOM
+elementos.value = "mudei o emerson";
+elementos.placeholder = "digite seu nome aqui";
+console.log(elementos.value);
+
+pegueName.placeholder = " pegue o nome";
+console.log(pegueName.placeholder);
+
+const paragrafo = document.querySelector(".paragra");
+
+// paragrafo.textContent= "novo texto";
+// paragrafo.innerText= "novo texto";
+paragrafo.innerHTML = "novo text <b> ola </b>.";
+
+console.log(paragrafo.textContent); //SO HTML
+console.log(paragrafo.innerText);   //LEVA EM CONTA O CSS
+console.log(paragrafo.innerHTML);   //TRAZ TUDO - permite adicionar HTML
+
